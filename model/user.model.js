a0254899211463a2b0eae8b5418e664908dd5ce3
adnan-wb <adnandev604@gmail.com>
2025-02-26 15:19:43 +0500

    flutter-app

--- model/user.model.js
+++ model/user.model.js
@@ -1,30 +1,39 @@
 const mongoose = require('mongoose');
 const db = require('../config/db');
+const bcrypt = require('bcrypt');
+
 
 const { Schema } = mongoose;
 
 
 const userSchema = new Schema({
-
-    // username: {
-    //     type: String,
-    //     required: true
-    // },
-
     email: {
         type: String,
         lowercase: true,
         required: true,
         unique: true
     },
-    passwowrd: {
+    password: {
         type: String,
         required: true
     }
 })
 
 
+userSchema.pre('save', async function () {
+    try {
+        var user = this;
+        const salt = await (bcrypt.genSalt(10));
+        const hashPass = await bcrypt.hash(user.password, salt);
+        user.password = hashPass;
+
+    } catch (error) {
+        throw error;
+
+    }
+});
 
 const UserModel = db.model('user', userSchema);
+
 
 module.exports = UserModel;
